f1b9b6af52ef01dbe0676052b46271f7e7f2bad0
Carlo van Beckhoven <carlovanb.cvb@gmail.com>
2023-10-17 19:53:12 +0200

    basic Flask code

--- flask_app.py
+++ flask_app.py
@@ -1,8 +1,9 @@
 from flask import Flask
 app=Flask(__name__)
-if __name__ == '__main__':
-    app.run(port=5000,debug=True)
 
 @app.route('/')
 def home():
-    return 'Hello world!'
+    return '<p>Mijn huiswerk voor les 10 heb ik veel te lang over gedaan omdat ik de voorgeprogrammeerde website van NHA probeerde te pimpen</p>'
+
+if __name__ == '__main__':
+    app.run(port=5000,debug=True)
